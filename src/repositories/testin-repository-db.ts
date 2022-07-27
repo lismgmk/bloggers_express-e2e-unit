b09f4faf042d08a1925b3d6c841efd6e53b74e64
vova <lismgmk2@gmail.com>
2022-07-28 00:43:33 +0300

    some fix-4

--- src/repositories/testin-repository-db.ts
+++ src/repositories/testin-repository-db.ts
@@ -6,10 +6,12 @@ export const testingRepositoryDB = {
     const resultPosts = await collections.posts?.deleteMany({});
     const resultComments = await collections.comments?.deleteMany({});
     const resultUsers = await collections.users?.deleteMany({});
+    const ipUsers = await collections.ipUsers?.deleteMany({});
     if (
       resultBloggers?.acknowledged &&
       resultComments?.acknowledged &&
       resultPosts?.acknowledged &&
+      ipUsers?.acknowledged &&
       resultUsers?.acknowledged
     ) {
       return true;
